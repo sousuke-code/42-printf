419c40f287fdf5c0c176265960768b075897976f
sousuke-code <msousuke1@gmail.com>
2024-11-12 22:31:29 +0900

    c指定子の出力作成

--- ft_printf.c
+++ ft_printf.c
@@ -1,43 +1,48 @@
+#include <stdarg.h>
 #include <stdio.h>
 #include <unistd.h>
-#include <stdarg.h>
 
-void ft_putchar(char c)
+void	ft_putchar(char c)
 {
-   write(1,&c,1);
+	write(1, &c, 1);
 }
 
-
-
-int ft_printf(const char *fmt, ...)
+int	ft_printf(const char *fmt, ...)
 {
-    va_list ap;
-    int n;
-    const char *start;
+	va_list		ap;
+	int			n;
+	const char	*start;
+	char		c;
 
-    n = 0;
-    if (fmt == NULL)
-       n = -1;
-    va_start(ap, fmt);
-    while(n >= 0 && *fmt)
-    {
-        start = fmt;
-        if (*start != '%')
-           ft_putchar(*fmt);
-           n++;
-         else
-         {
-            if()
-         }
-        fmt++;
-    }
-    va_end(ap);
-    return (n);
+	n = 0;
+	if (fmt == NULL)
+		n = -1;
+	va_start(ap, fmt);
+	while (n >= 0 && *fmt)
+	{
+		start = fmt;
+		if (*start != '%')
+		{
+			ft_putchar(*fmt);
+			n++;
+		}
+		else
+		{
+			fmt++;
+			if (*fmt == 'c')
+			{
+				c = (char)va_arg(ap, int);
+				ft_putchar(c);
+				n++;
+			}
+		}
+		fmt++;
+	}
+	va_end(ap);
+	return (n);
 }
 
-
-
-int main(void)
+int	main(void)
 {
-   printf("%c","acccc");
+	ft_printf("%c", 'a');
 }
