3b76c2dc47fd58c0426cfc24cbe08c061cefea30
sousuke-code <msousuke1@gmail.com>
2024-11-12 22:24:24 +0900

    指定子なしの出力作成

--- ft_printf.c
+++ ft_printf.c
@@ -1,25 +1,43 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+void ft_putchar(char c)
+{
+   write(1,&c,1);
+}
+
 
 
 int ft_printf(const char *fmt, ...)
 {
     va_list ap;
-    int n:
+    int n;
     const char *start;
 
     n = 0;
     if (fmt == NULL)
        n = -1;
-    va_strat(ap, fmt);
+    va_start(ap, fmt);
     while(n >= 0 && *fmt)
     {
         start = fmt;
         if (*start != '%')
-           // %ではない場合は普通に出力する
-        else
-           // %書きた場合に指定子による場合分け関数に移行する。
-
+           ft_putchar(*fmt);
+           n++;
+         else
+         {
+            if()
+         }
         fmt++;
     }
     va_end(ap);
     return (n);
+}
+
+
+
+int main(void)
+{
+   printf("%c","acccc");
 }
